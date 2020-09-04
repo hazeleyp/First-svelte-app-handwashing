f0c787545169cbff8233bd135ee8aaf0157bd174
Haze Pascoe <hazeleyp@gmail.com>
2020-09-04 16:43:32 +1000

    Adding CSS

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Timer from './Timer.svelte';
+    import Howto from './Howto.svelte';
+</script>
+
+<h1>Handwashing App</h1>
+
+<Howto />
+<Timer />
+
+<style>
+    h1 {
+        text-align: center;
+    }
+</style>
---
+++ src/Timer.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Counter from './Counter.svelte'
+</script>
+
+<p>Timer</p>
+
+<Counter />
